aaf7db6ab6534634ee59cab7e3487b237361d6a8
emilyqian21 <47677144+emilyqian21@users.noreply.github.com>
2026-06-14 17:57:39 -0700

    Add: two-integer-sum-ii - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-2.py
@@ -0,0 +1,34 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        #two pointer
+        l = 0
+        r = len(numbers) -1 
+
+        while l<r:
+            total_sum = numbers[l] + numbers[r]
+            if total_sum == target:
+                return [l+1, r+1]
+            elif total_sum < target:
+                l += 1
+            else:
+                r -= 1
+        return []
+        #time: O(n)
+        #space: O(1)
+
+
+
+
+
+        # # hashset solution 
+        # visited = {}
+        # for i in range(len(numbers)):
+        #     complement = target - numbers[i]
+        #     if complement in visited:
+        #         return [visited[complement]+1,i+1] #因为一定是先看到complement,再看到现在的i
+        #     visited[numbers[i]] = i 
+        # #time: O(N)
+        # #space: O(N)
+
+
+            
