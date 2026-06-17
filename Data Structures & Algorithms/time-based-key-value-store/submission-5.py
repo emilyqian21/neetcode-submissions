b938bcb89f06e4d27abbe57fc8aff11d3710e304
emilyqian21 <47677144+emilyqian21@users.noreply.github.com>
2026-06-17 16:05:27 -0700

    Add: time-based-key-value-store - submission-5

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-5.py
@@ -0,0 +1,36 @@
+class TimeMap:
+
+    def __init__(self):
+        self.store = defaultdict(list) # store： [[val,timestamp]]
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.store[key].append([value,timestamp])
+
+    def get(self, key: str, timestamp: int) -> str:
+
+
+        res_space = self.store[key]
+        if not res_space:
+            return ""
+        l = 0 
+        r = len(res_space) - 1
+        
+        while l <= r:
+            mid = l + (r-l)//2
+
+            if res_space[mid][1] <= timestamp:
+                l = mid + 1
+            ##上面这行等同于下面的：
+            # if res_space[mid][1] == timestamp:
+            #     return res_space[mid][0]
+            # elif res_space[mid][1] < timestamp:
+            #     l = mid + 1
+            else:
+                r = mid - 1
+
+            if r < 0: #保证r != -1
+                return ""
+
+        return res_space[r][0]
+        
+        
