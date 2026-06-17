5bee31eb0c0c72030518848c237fa4efb1ce315d
emilyqian21 <47677144+emilyqian21@users.noreply.github.com>
2026-06-16 21:58:07 -0700

    Add: find-minimum-in-rotated-sorted-array - submission-1

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        l = 0 
+        r = len(nums) -1 
+
+        while l < r: #因为答案就是 l == r 的时候
+            mid = l + (r-l)//2
+
+            if nums[mid] < nums[r]: # 在小数区,单调递增，那么最小的数可能是MID 也可能是mid左边
+                r = mid 
+            else: # 当nums[mid] == nums[r]不可能因为无重复数字，所以只有nums[mid] > nums[r]，在大数区，最小的数在mid右边
+                l = mid + 1
+
+        return nums[l]
+        # time: 0(log n )
+        # space: O(1)
+
