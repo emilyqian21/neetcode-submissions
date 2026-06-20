f630bf5092160baf95db67d452bd991abd124f63
emilyqian21 <47677144+emilyqian21@users.noreply.github.com>
2026-06-19 21:14:17 -0700

    Add: reverse-a-linked-list - submission-1

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-1.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+
+        newhead = self.reverseList(head.next)  #
+        head.next.next = head
+        head.next = None
+
+        return newhead
+        # time: O(N)
+        # space: O（N）,虽然指针是O(1)但是有 recursive call stack， call stack是 O (N)
+## Recursive Reverse Linked List
+# 永远只是在修改指针，只有一个linked list，不会创建copy
+# 每个node存两个variable,一个是value，一个是下一个连接的点，比如 1->2->3，当head 指向1, 进行了rseverselist(head.next)后， new head--> 3-->2-->None, 
+#但是 对于node 1来说，它存的value是1，指的下一个还是2，所以 就变成了
+#  3 -> 2 -> None
+#      ^
+#      |
+# 1 ----
+
