31777d45bb7f3a5eeb5b10862156c7227bebb9ba
emilyqian21 <47677144+emilyqian21@users.noreply.github.com>
2026-06-23 21:55:59 -0700

    Add: kth-smallest-integer-in-bst - submission-2

---
+++ Data Structures & Algorithms/kth-smallest-integer-in-bst/submission-2.py
@@ -0,0 +1,57 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        
+        #recursive dfs, 简洁写法
+        self.k = k
+        self.res = None
+
+        def inorder(node):
+            if not node or self.res is not None:
+                return
+
+            inorder(node.left)
+
+            self.k -= 1
+            if self.k == 0:
+                self.res = node.val
+                return
+
+            inorder(node.right)
+
+        inorder(root)
+        return self.res
+
+        # #recursive dfs, 最完整的逻辑， time O(n) space O(n)
+        # self.count = 0
+
+        # def dfs(node):
+        #     # base case: 空节点里找不到答案
+        #     if not node:
+        #         return None
+
+        #     # 1. 先去左子树找
+        #     left = dfs(node.left)
+        #     if left is not None:
+        #         return left
+
+        #     # 2. 处理当前节点
+        #     self.count += 1
+        #     if self.count == k:
+        #         return node.val
+
+        #     # 3. 再去右子树找
+        #     right = dfs(node.right)
+        #     if right is not None:
+        #         return right
+
+        #     # 4. 左、当前、右都没找到
+        #     return None
+
+        # return dfs(root)
