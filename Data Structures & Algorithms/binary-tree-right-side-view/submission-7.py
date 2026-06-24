a13bd7ce4d5d41cd6f235a87854d5f66f2f2f55a
emilyqian21 <47677144+emilyqian21@users.noreply.github.com>
2026-06-23 17:59:17 -0700

    Add: binary-tree-right-side-view - submission-7

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-7.py
@@ -0,0 +1,83 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        # self.res = []
+        res = []
+        def dfs(node,level):
+            #base case 
+            if not node:
+                return 0
+            
+            #curr node
+            if level == len(res): #self.res
+                res.append(node.val)
+                # self.res.append(node.val)
+            
+            # right tree first, then left
+            dfs(node.right,level+1)
+            dfs(node.left,level+1)
+        
+        dfs(root, 0)
+        return res #self.res
+
+
+
+
+
+
+
+
+
+
+
+
+        # #bfs精简版： time O(n) space O(n)
+        # if not root:
+        #     return []
+        # global_res = []
+        # queue = deque([root])
+
+        # while queue:
+        #     rightmost = None
+        #     for _ in range(len(queue)):
+        #         curr = queue.popleft()
+        #         rightmost = curr.val #或者 if i == level_len - 1: res.append(curr.val)
+        #         if curr.left:
+        #             queue.append(curr.left)
+        #         if curr.right:
+        #             queue.append(curr.right)
+        #     global_res.append(rightmost)
+        # return global_res
+
+
+
+
+        # bfs ：只加这个level的最后一个数字
+        # #edge case:
+        # if not root:
+        #     return []
+
+        # global_res = []
+        # queue = deque([root])
+
+        # while queue:
+        #     level_res = []
+        #     for _ in range(len(queue)): #level 
+        #         curr = queue.popleft()
+        #         level_res.append(curr.val)
+
+        #         # add left and right to queue
+        #         if curr.left:
+        #             queue.append(curr.left)
+        #         if curr.right:
+        #             queue.append(curr.right)
+        #     #end of this level
+        #     global_res.append(level_res[-1])
+
+        # return global_res
