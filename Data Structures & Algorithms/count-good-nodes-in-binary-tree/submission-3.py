8de8387c7d8df1ceaa1d2009747e7d22a2700cfa
emilyqian21 <47677144+emilyqian21@users.noreply.github.com>
2026-06-23 18:24:56 -0700

    Add: count-good-nodes-in-binary-tree - submission-3

---
+++ Data Structures & Algorithms/count-good-nodes-in-binary-tree/submission-3.py
@@ -0,0 +1,68 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        #BFS: queue里存tuple(node, path_max) time: O(n) space: O(n)
+        #edge case:
+        if not root:
+            return 
+        queue = deque([(root,root.val)])
+        res = 0
+
+        while queue:
+            curr,path_max = queue.popleft()
+            path_max = max(path_max, curr.val)
+            if curr.val >= path_max:
+                res += 1
+            if curr.left:
+                queue.append((curr.left,path_max))
+            if curr.right:
+                queue.append((curr.right, path_max))
+        return res
+
+
+
+
+
+
+
+
+        # #dfs: 局部变量 time O(N) space O(N)
+        # def dfs(node,maxval):
+        #     #base case
+        #     if not node:
+        #         return 0
+            
+        #     #preorder 
+        #     if node.val >= maxval:
+        #         res = 1 #这里的res是局部变量，每次call dfs都会创一个新的res，但指代的不是同一个
+        #     else:
+        #         res = 0 
+        #     maxval = max(maxval, node.val)
+        #     res += dfs(node.left,maxval)
+        #     res += dfs(node.right, maxval)
+        
+        #     return res
+        # return dfs(root,root.val)
+
+        #  #dfs: 全局变量
+        # self.count = 0
+        # def dfs(node,maxval):
+        #     #base case
+        #     if not node:
+        #         return 0
+            
+        #     #preorder 
+        #     if node.val >= maxval:
+        #         self.count += 1
+        #     maxval = max(maxval, node.val)
+        #     dfs(node.left,maxval)
+        #     dfs(node.right, maxval)
+        
+        # dfs(root,root.val)
+        # return self.count
