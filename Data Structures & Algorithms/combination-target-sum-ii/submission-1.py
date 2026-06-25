1a7ea7687deae803d9fe1a8bc9b0329ef41a80ca
emilyqian21 <47677144+emilyqian21@users.noreply.github.com>
2026-06-24 17:28:56 -0700

    Add: combination-target-sum-ii - submission-1

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-1.py
@@ -0,0 +1,30 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        #time O(n^2n) space O(n)
+        res = []
+        candidates.sort()
+        def dfs(start,total,path):
+            #base case
+            if total == target:
+                res.append(path.copy())
+                return 
+            if total > target:
+                return 
+            
+            #process current node
+            for i in range(start, len(candidates)):
+                
+                #pruning
+                if candidates[i] > target:
+                    break
+                if i > start and candidates[i] == candidates[i-1]: # 如果有重复的，就跳过重复的
+                    continue
+                #process current node
+                path.append(candidates[i])
+                # keep exploring 
+                dfs(i+1, total + candidates[i], path)
+                # pop
+                path.pop()
+
+        dfs(0,0,[])
+        return res
