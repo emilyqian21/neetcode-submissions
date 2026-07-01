1f2ff815a41aa4a4ed3822baba629116fed4866e
emilyqian21 <47677144+emilyqian21@users.noreply.github.com>
2026-07-01 09:07:40 -0700

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        #bottom up dp, tabulation 
+        # time : O(n)
+        # space: O(n)
+        n = len(nums)
+        dp = [0] * n 
+        if n == 1:
+            return nums[0]
+
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1]) # max(not rob this rob house before, rob this and two house before)
+
+        for i in range(2,n):
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i]) # max(not rob this rob house before = keep dp[i-1], rob this and two house before = dp[i-2] + nums[i])
+
+        return dp[-1]
