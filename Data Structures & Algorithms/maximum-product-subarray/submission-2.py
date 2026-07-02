564e66c2e681bf594440adacb673a67dc175b364
emilyqian21 <47677144+emilyqian21@users.noreply.github.com>
2026-07-01 21:10:10 -0700

    Add: maximum-product-subarray - submission-2

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        res = nums[0]
+        cur_max = 1
+        cur_min = 1
+
+        for n in nums:
+            temp_max = cur_max
+            cur_max = max(cur_max * n, cur_min *n, n)
+            cur_min = min(temp_max * n, cur_min *n, n) # extend previous max, extend previous min,start a new subarray
+            res = max(res, cur_max)
+        return res
+
