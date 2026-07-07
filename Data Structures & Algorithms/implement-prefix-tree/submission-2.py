b5d84fda0226135f34dc65e38944a73615333399
emilyqian21 <47677144+emilyqian21@users.noreply.github.com>
2026-07-07 13:45:33 -0700

    Add: implement-prefix-tree - submission-2

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-2.py
@@ -0,0 +1,43 @@
+# Time: O(n) n = length of the string
+
+# Space: O(t) t = total number of TrieNodes created 
+
+class TrieNode:
+    def __init__(self):
+        self.children = {} # 'a':node(), 'b':node()
+        self.endOfWord = False
+
+class PrefixTree:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        curr = self.root
+        for c in word:
+            if c in curr.children:
+                curr = curr.children[c]
+            else:
+                curr.children[c] = TrieNode()
+                curr = curr.children[c]
+        #end of insert
+        curr.endOfWord = True
+
+    def search(self, word: str) -> bool:
+        curr = self.root
+        for c in word:
+            if c in curr.children:
+                curr = curr.children[c]
+            else:
+                return False
+        return curr.endOfWord
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+        for c in prefix:
+            if c in curr.children:
+                curr = curr.children[c]
+            else:
+                return False
+        return True
+        
