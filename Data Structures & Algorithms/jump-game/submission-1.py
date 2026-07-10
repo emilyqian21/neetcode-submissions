59cfc7b9209d26e43601e008f568837c8978a318
emilyqian21 <47677144+emilyqian21@users.noreply.github.com>
2026-07-09 22:24:45 -0700

    Add: jump-game - submission-1

---
+++ Data Structures & Algorithms/jump-game/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        n = len(nums)
+        target = n - 1 # index, 最终我们要reach 0
+
+        for i in range(n-1, -1, -1): #从后往前traverse
+            max_jump = nums[i] # 在i可以跳最远的步数
+            if i + max_jump >= target: #如果在i，可以达到target,那么新的target就是i,我们只要到达i就可以了，因为到达i后一定能到target
+                target = i 
+                            # 如果当前 i 不能到达 target，不代表更前面的 index 不可以。你应该继续往前找。
+        return target == 0
