98fbd57064101df97f99b6b0ff1638d05b62bb65
emilyqian21 <47677144+emilyqian21@users.noreply.github.com>
2026-07-10 17:11:34 -0700

    Add: jump-game-ii - submission-3

---
+++ Data Structures & Algorithms/jump-game-ii/submission-3.py
@@ -0,0 +1,18 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        # time: O(n) 
+        # space: O(1)
+        # 探索每一“跳”的所有可能性，找到最远的边界
+        current_jump_farest_point= 0  # 这一跳最远能到哪里
+        scout_next_jump_farest_point = 0 # 站在每个现在能跳到的节点上，看下一跳最远到哪里
+        jump = 0 
+
+        for i in range(len(nums) - 1):
+            # if i make the jump here, my current jump farest point is to 
+            scout_next_jump_farest_point = max(nums[i]+i, scout_next_jump_farest_point) # scout一直在变化
+
+            if i == current_jump_farest_point: # if i already reach the boundry of the farest point of this jump
+                current_jump_farest_point = scout_next_jump_farest_point # i've already searched all the starting position for next jump, the scout process ends; i can determine the current_jump_farest_point
+                jump += 1 
+
+        return jump
