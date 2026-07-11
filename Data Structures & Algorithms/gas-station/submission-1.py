30420e0bec24346f479131c2317837d7e23b73d9
emilyqian21 <47677144+emilyqian21@users.noreply.github.com>
2026-07-10 21:14:53 -0700

    Add: gas-station - submission-1

---
+++ Data Structures & Algorithms/gas-station/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        # time: O(n)
+        # space: O(1)
+        if sum(gas) < sum(cost):
+            return -1
+        start = 0 
+        total = 0
+        
+        for i in range(len(gas)):
+            diff = gas[i] - cost[i]
+            total += diff
+            
+            if total < 0: # then this can't be the start point. the start point total will never be below 0 
+                total = 0 
+                start = i + 1
+        return start
