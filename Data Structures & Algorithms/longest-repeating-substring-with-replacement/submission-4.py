db90c60d0115ac1ece6a73bf9448f10235968359
emilyqian21 <47677144+emilyqian21@users.noreply.github.com>
2026-07-16 16:33:32 -0700

    Add: longest-repeating-substring-with-replacement - submission-4

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-4.py
@@ -0,0 +1,18 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        #different characters as valid condition?
+        # 窗口合法的condition： 除去frequncy最多的字符外，剩下的字符数量 < K
+        l = 0
+        freq = {}
+        max_res = 0
+
+        for r in range(len(s)):
+            freq[s[r]] = freq.get(s[r],0) + 1
+
+            while freq and (r - l + 1) - max(freq.values()) > k: # 剩下的非最多的数字>k个，需要shrink，直到合法为止
+                freq[s[l]] = freq.get(s[l],0) - 1
+                l += 1
+            length = r - l + 1
+            max_res = max(max_res, length)
+
+        return max_res
