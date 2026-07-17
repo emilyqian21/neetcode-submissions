0e3226395cfb430bcf59e62b79115cbd6bdb450d
emilyqian21 <47677144+emilyqian21@users.noreply.github.com>
2026-07-16 18:05:21 -0700

    Add: minimum-window-with-characters - submission-1

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-1.py
@@ -0,0 +1,32 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        # need, have
+
+        # time: O(n)
+        # space: O(n)
+
+        # counter of t
+        count_t = collections.Counter(t)
+        count_window = {}
+        need = len(count_t) # unique characters in t
+        have = 0 # match; if have == need, then we found all the characters we need
+        res = [-1,-1] # index of the subarray
+        res_len = float('inf')
+        l = 0 
+        for r in range(len(s)):
+            count_window[s[r]] = count_window.get(s[r],0) + 1
+            if s[r] in count_t and count_window[s[r]] == count_t[s[r]]:
+                have += 1
+            while need == have: 
+                # record the answer
+                if r - l + 1< res_len:
+                    res = [l,r]
+                    res_len = r - l + 1
+                # try to shrink 
+                #update have 
+                if s[l] in count_t and count_window[s[l]] == count_t[s[l]]:
+                    have -= 1
+                count_window[s[l]] -= 1
+                l += 1
+        return s[res[0]:res[1] + 1] if res_len != float('inf') else ""
+
