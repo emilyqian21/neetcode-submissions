46e39b7211109df6fbb5e60ecd51b1c3c40925da
emilyqian21 <47677144+emilyqian21@users.noreply.github.com>
2026-07-18 21:54:45 -0700

    Add: find-duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-2.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        # linked list, cycle dectection, floyd's algo(fast and slow pointers)
+        # fast = 0, slow = 0, fast走两步，fast和slow相遇的地方，是离cycle 起点X的位置
+        # 再开启一个slow2 = 0, slow和slow2一起走，两者相遇的地方，就是cycle的起点 
+
+        fast = 0 
+        slow = 0
+
+        while True:
+            fast = nums[nums[fast]]
+            slow = nums[slow]
+
+            if fast == slow:
+                break
+        
+        slow2 = 0
+        while True:
+            slow = nums[slow]
+            slow2 = nums[slow2]
+            if slow == slow2:
+                return slow
