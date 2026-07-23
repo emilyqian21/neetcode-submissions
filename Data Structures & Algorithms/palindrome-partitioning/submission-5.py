69b00ea9e96f0bdbc477df3ee0eb93cfb943f559
emilyqian21 <47677144+emilyqian21@users.noreply.github.com>
2026-07-23 11:02:57 -0700

    Add: palindrome-partitioning - submission-5

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-5.py
@@ -0,0 +1,35 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        # Time: O(n · 2ⁿ). There are 2ⁿ possible partitions of the string in the worst case, and for each partition we may spend up to O(n) time checking if substrings are palindromes.
+        # Space:auxiliary O(n); output  O(n · 2ⁿ)
+
+        res = []
+        def ispalindrome(s):
+            l = 0
+            r = len(s)-1
+            while l < r:
+                if s[l] != s[r]:
+                    return False
+                l += 1
+                r -= 1
+            return True
+
+        def dfs(start,path):
+            # record answer when necessary: when start == len(s)
+            if start == len(s):
+                res.append(path.copy())
+                return
+            
+            # explore all possibilities
+            # make decision
+            for i in range(start,len(s)):
+                if ispalindrome(s[start:i+1]):
+                    path.append(s[start:i+1])
+                    #further extend the path by exploring other following options
+                    dfs(i+1,path)
+
+                    # undo the choice
+                    path.pop()
+            return
+        dfs(0,[])
+        return res
