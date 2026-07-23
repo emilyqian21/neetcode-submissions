45dc0b9f3affabdf63d7540854493e144071ead3
emilyqian21 <47677144+emilyqian21@users.noreply.github.com>
2026-07-22 21:37:04 -0700

    Add: generate-parentheses - submission-3

---
+++ Data Structures & Algorithms/generate-parentheses/submission-3.py
@@ -0,0 +1,30 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        path = []
+        choice = ["(",")"]
+
+        def dfs(open_count, close_count):
+            # if valid record answer
+            if open_count == n and close_count == n:
+                res.append("".join(path.copy()))
+                return
+            
+            for i in range(len(choice)):
+                # make the choice 
+                #pruning
+                if open_count > n or close_count > open_count:
+                    # invalid
+                    return
+                else:
+                    path.append(choice[i])
+                    if choice[i] == "(":
+                        dfs(open_count + 1, close_count)
+                    elif choice[i] == ")":
+                        dfs(open_count, close_count + 1)
+
+                # undo
+                path.pop()
+
+        dfs(0,0)
+        return res
