661ef8dcf657ac78aee4fb875fb04aa334b47548
emilyqian21 <47677144+emilyqian21@users.noreply.github.com>
2026-07-31 15:48:58 -0700

    Add: foreign-dictionary - submission-5

---
+++ Data Structures & Algorithms/foreign-dictionary/submission-5.py
@@ -0,0 +1,56 @@
+class Solution:
+    def foreignDictionary(self, words: List[str]) -> str:
+        # topological sort ( postorder dfs?)
+        # Time Complexity:
+        # O(C + V + E)
+
+        # where:
+        # - C = total number of characters in all words
+        # - V = number of unique characters
+        # - E = number of precedence relationships
+
+        # Building the graph takes O(C), and the DFS-based topological sort visits each character and edge once.
+
+        # Space Complexity:
+        # O(V + E)
+
+        #for the graph, visited/status map, recursion stack, and output.
+
+        word2neighbor = { c: set() for word in words for c in word}
+        for i in range(len(words) - 1):
+            w1, w2 = words[i], words[i + 1]
+            minlen = min(len(w1), len(w2))
+            # edge case
+            if w1[:minlen] == w2[ :minlen] and len(w1) > len(w2):
+                return ""
+            for j in range(minlen):
+                if w1[j] != w2[j]:
+                    word2neighbor[w1[j]].add(w2[j]) # find the first different character
+                    break 
+
+
+        # topolocigal sort
+        status = {} # True = visited and in the loop  # False = visited but not in the loop
+        output = []
+        def dfs(c):
+            # base case
+            if c in status:
+                return status[c] # return the recorded status. if status[c] == True, 说明是loop
+            
+            # process current node 
+            status[c] = True 
+
+            for nei in word2neighbor[c]:
+                if dfs(nei):
+                    return True
+
+
+            status[c] = False
+            output.append(c)
+            return False
+        
+        for c in word2neighbor: # 易错点：遍历的是dict的key
+            if dfs(c):
+                return ""
+
+        return "".join(output[::-1])
