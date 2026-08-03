2aecd77c7508ddf31c389d33c223ac0e30f963c7
emilyqian21 <47677144+emilyqian21@users.noreply.github.com>
2026-08-02 17:58:09 -0700

    Add: kth-largest-integer-in-a-stream - submission-3

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-3.py
@@ -0,0 +1,15 @@
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        heapq.heapify(nums)  # 易错点：heapify是直接inplace修改，是return None的
+        self.heap = nums
+        self.k = k
+
+    def add(self, val: int) -> int:
+        # 1. add into the heap
+        heapq.heappush(self.heap, val)
+        # 2. pop 
+        while len(self.heap) > self.k:
+            heapq.heappop(self.heap)
+        # 3.return kth largest value
+        return self.heap[0] # the top of the minheap is the smallest 
