c8bf75c24b24d0bc0a3dabb00540d55e77d61a87
emilyqian21 <47677144+emilyqian21@users.noreply.github.com>
2026-08-02 19:32:21 -0700

    Add: k-closest-points-to-origin - submission-4

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-4.py
@@ -0,0 +1,34 @@
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+            # time: O(nlogn) ,less efficient than maxheap O(nlogk)
+            # space: O(n), less efficient than maxheap O(k)
+        # def distance(x,y): # calculate the distance from x,y to origin(0,0)
+        #     return ((x - 0)**2 + (y - 0)**2)**0.5
+        
+        # minheap = []
+        # for x,y in points:
+        #     d = distance(x,y)
+        #     heapq.heappush(minheap, (d,x,y))
+        # # find the smallest top k 
+        # output = []
+        # while len(output) < k:
+        #     d,x,y = heapq.heappop(minheap)
+        #     output.append([x,y])
+        # return output
+
+        # max heap
+        # time: O(nlogk)
+        # space: O(k)
+        def distance(x,y):
+            return ((x - 0)**2 + (y - 0)**2)**0.5
+        heap = []
+        for x,y in points:
+            d =  distance(x,y)
+            heapq.heappush(heap,(-d,x,y))
+            while len(heap) > k:
+                heapq.heappop(heap)
+        res = []
+        while heap:
+            d,x,y = heapq.heappop(heap)
+            res.append([x,y])
+        return res
