59ab21eb2021b9699350caad8dc70617fb8938fa
emilyqian21 <47677144+emilyqian21@users.noreply.github.com>
2026-08-04 11:28:04 -0700

    Add: meeting-schedule - submission-4

---
+++ Data Structures & Algorithms/meeting-schedule/submission-4.py
@@ -0,0 +1,24 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        # time: O(nlogn)
+        # Space: O(n) in Python because list.sort() (Timsort) can use O(n) auxiliary space. The algorithm itself uses only O(1) additional variables.
+        # edge case
+        if not intervals:
+            return True
+        #sort and detect overlap
+        intervals.sort(key = lambda x : x.start) # here class is interval, self.start, slef.end
+        preend = intervals[0].end
+
+        for interval in intervals[1:]:
+            if interval.start < preend: # overlap
+                return False
+            preend = interval.end
+        return True
