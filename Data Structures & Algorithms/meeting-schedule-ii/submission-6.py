7c1d99fc40f9c019f3bb0d5acfd019a207b895b4
emilyqian21 <47677144+emilyqian21@users.noreply.github.com>
2026-08-04 11:35:08 -0700

    Add: meeting-schedule-ii - submission-6

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-6.py
@@ -0,0 +1,25 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        #edge case
+        if not intervals:
+            return 0
+        
+        heap = []
+        intervals.sort(key = lambda x: x.start)
+        heapq.heappush(heap, intervals[0].end) # rank by the end time 
+
+        for interval in intervals[1:]:
+            if heap and interval.start < heap[0]: # overlap
+                heapq.heappush(heap, interval.end) # start a new room
+            else:
+                heapq.heappop(heap) # reuse the room. so drop the room fist
+                heapq.heappush(heap, interval.end) # update the room info
+        return len(heap) 
