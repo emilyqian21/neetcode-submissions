ca7fb78798870399b263946dd4428ee587a86239
emilyqian21 <47677144+emilyqian21@users.noreply.github.com>
2026-08-04 11:42:31 -0700

    Add: maximum-subarray - submission-4

---
+++ Data Structures & Algorithms/maximum-subarray/submission-4.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        # kadane's algo
+        #edge case 
+        if not nums:
+            return 0
+        # main code
+        cursum = nums[0]
+        maxsum = nums[0]
+
+        for n in nums[1:]:
+            # greedy: either add it to current sum or start fresh
+            cursum = max((cursum + n), n)
+            maxsum = max(maxsum, cursum)
+        return maxsum
