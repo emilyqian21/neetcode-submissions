bba1071873200647fa78322ab7c11363d450372b
emilyqian21 <47677144+emilyqian21@users.noreply.github.com>
2026-08-04 12:04:21 -0700

    Add: jump-game - submission-3

---
+++ Data Structures & Algorithms/jump-game/submission-3.py
@@ -0,0 +1,8 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        # greedy: start from the end, try to move the goal backwards as far as possible
+        goal = len(nums) - 1
+        for i in range(len(nums)-1, -1, -1):  # taverse backwards 
+            if i + nums[i] >= goal: # if at position i it can jump to the goal
+                goal = i # we move the goal to i. we only need to jump to i then we can garantee we can reach the final goal
+        return goal == 0
