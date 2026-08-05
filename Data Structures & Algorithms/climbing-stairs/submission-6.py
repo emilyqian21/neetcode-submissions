d254d168c435c66e3d691bd55244196e36fd27ad
emilyqian21 <47677144+emilyqian21@users.noreply.github.com>
2026-08-04 21:16:56 -0700

    Add: climbing-stairs - submission-6

---
+++ Data Structures & Algorithms/climbing-stairs/submission-6.py
@@ -0,0 +1,18 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        # time: O(n)
+        # space: O(1)
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        
+        prev = 1
+        cur = 2
+
+        for n in range(3,n+1):
+            temp_prev = prev
+            prev = cur
+            cur = temp_prev + cur
+
+        return cur
