90352ed859bf382b13ee1bd72c9230263d6c583d
emilyqian21 <47677144+emilyqian21@users.noreply.github.com>
2026-08-04 19:57:08 -0700

    Add: merge-triplets-to-form-target - submission-2

---
+++ Data Structures & Algorithms/merge-triplets-to-form-target/submission-2.py
@@ -0,0 +1,14 @@
+class Solution:
+    def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
+        # time: O(n)
+        # space: O(1)
+        # if the value in triplet is larger than that in target, this triplet can't be the answer
+        achievable = set()
+        for t in triplets:
+            if t[0] > target[0] or t[1] > target[1] or t[2] > target[2]:
+                continue
+            else:
+                for i,v in enumerate(t):
+                    if v == target[i]:
+                        achievable.add(v)
+        return len(achievable) == len(set(target))
