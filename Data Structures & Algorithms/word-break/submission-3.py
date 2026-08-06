cec7d74f515c8e4628510ab7d862b51a897a0df5
emilyqian21 <47677144+emilyqian21@users.noreply.github.com>
2026-08-06 11:03:01 -0700

    Add: word-break - submission-3

---
+++ Data Structures & Algorithms/word-break/submission-3.py
@@ -0,0 +1,13 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        wordDict = set(wordDict) # set loopup is O(1), list is O(n)
+        n = len(s)
+        dp = [False] * (n + 1) # prefix
+        dp[0] = True # we can use 0 word in wordDict to form string
+
+        for i in range(1, len(dp)):
+            for j in range(i):
+                if dp[j] and s[j:i] in wordDict:
+                    dp[i] = True
+                    break # no need to further explore j
+        return dp[-1]
