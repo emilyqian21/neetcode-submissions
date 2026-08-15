cb39745bc3cc0ca56b9c70b2696459a2a89325ad
emilyqian21 <47677144+emilyqian21@users.noreply.github.com>
2026-08-14 21:14:01 -0700

    Add: combination-target-sum - submission-6

---
+++ Data Structures & Algorithms/combination-target-sum/submission-6.py
@@ -0,0 +1,29 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        # time: ?
+        # sapce: ?
+        res = []
+        nums.sort()
+
+        def dfs(start, pathsum, path):
+            # when to return 
+            if pathsum == target:
+                # find the answer     
+                res.append(path.copy())
+                return
+            
+            # main logic
+            for i in range(start, len(nums)):       
+                if nums[i] + pathsum > target:
+                    break
+
+                # explore 
+                path.append(nums[i])    
+                dfs(i, pathsum + nums[i], path)
+
+                # undo
+                path.pop() # 用pop，不要用remove(nums[i])
+            return 
+        
+        dfs(0,0,[])
+        return res
