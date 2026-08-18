b8d419f9d2609232b2b2d3721bd217b7c75728c7
emilyqian21 <47677144+emilyqian21@users.noreply.github.com>
2026-08-17 21:08:16 -0700

    Add: course-schedule - submission-12

---
+++ Data Structures & Algorithms/course-schedule/submission-12.py
@@ -0,0 +1,41 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # time: O( V + E)
+        # space: O( V + E)
+        course2pre = {}
+        for c,p in prerequisites:
+            if c not in course2pre:
+                course2pre[c] = []
+            course2pre[c].append(p) # {1:[0,2],2:[3]} {0:[1],1:[0]} {0:[1]}
+        
+    
+
+        def dfs(c, path): # return boolean; if we can take course c; path records all the nodes before current node; 
+            # base case
+            if c not in course2pre or not course2pre[c]:
+                return True
+            if c in path:
+                return False
+
+            # process current node
+            path.add(c)
+            # explore recurisvely, continue this path
+            for pre in course2pre[c]:
+                if not dfs(pre, path):
+                    path.remove(c)
+                    return False
+
+            # 这条path验证结束 (backtrack)
+            path.remove(c)
+            #memoization for optimization
+            course2pre[c] = [] # 这个课可以完成，下次不用跑了
+            return True
+
+        for c in range(numCourses):
+            if not dfs(c,set()): # 易错点，不是dfs(0),而是要检查所有课。因为可能有点课有loop，有的课没有loop
+                return False
+        return True
+
+
+            
+        
