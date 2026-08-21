58b1b576eab2ffcdec87d36a92595200b2d419e3
emilyqian21 <47677144+emilyqian21@users.noreply.github.com>
2026-08-20 21:53:46 -0700

    Add: generate-parentheses - submission-10

---
+++ Data Structures & Algorithms/generate-parentheses/submission-10.py
@@ -0,0 +1,34 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = []
+        def dfs(start, path, open_n, close_n):
+            # traverse all possible path start at s[start:]
+            # base case
+            if start == n*2 and open_n == close_n: # traverse all position and valid
+                res.append(''.join(path))
+                return
+            if start == n*2:
+                return
+            
+            # main logic
+            # choice space
+            
+            # make choice
+            if open_n + 1 >= close_n: # invalid
+                path.append("(")
+                dfs(start + 1, path, open_n + 1, close_n)
+                path.pop()
+    
+            # make choice
+            if open_n >= close_n + 1:
+                path.append(")")
+                dfs(start + 1, path, open_n, close_n + 1)
+                path.pop()
+
+            return res
+            
+        dfs(0,[],0,0)
+        return res
+
+                
+                
