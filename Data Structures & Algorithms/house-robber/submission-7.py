f4898f74b387ac6ffc0e9afb93f3c640e30ee676
emilyqian21 <47677144+emilyqian21@users.noreply.github.com>
2026-08-24 21:35:16 -0700

    Add: house-robber - submission-7

---
+++ Data Structures & Algorithms/house-robber/submission-7.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # edge case
+        if not nums:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
+            
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+
+        for i in range(2, len(dp)):
+            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1]) # option1 steal option2 not steal
+        return dp[-1]
