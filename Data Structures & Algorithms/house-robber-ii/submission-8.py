ea749896ba93c9f7f5ee9f6bd2f48241053be159
emilyqian21 <47677144+emilyqian21@users.noreply.github.com>
2026-08-24 21:40:49 -0700

    Add: house-robber-ii - submission-8

---
+++ Data Structures & Algorithms/house-robber-ii/submission-8.py
@@ -0,0 +1,25 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        def houserob_helper(h):
+            # edge case
+            if not h:
+                return 0
+            if len(h) == 1:
+                return h[0]
+
+            dp = [0] * len(h)
+            dp[0] = h[0]
+            dp[1] = max(h[0], h[1])
+            for i in range(2, len(dp)):
+            # main logic
+                dp[i] = max(dp[i - 2] + h[i], dp[i - 1])
+            return dp[-1]
+        
+        # edge case
+        if not nums:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
+
+        return max(houserob_helper(nums[:-1]), houserob_helper(nums[1:]))
+            
