a9d5059dbccf71f968e5c8520970246b698fa872
emilyqian21 <47677144+emilyqian21@users.noreply.github.com>
2026-09-01 13:27:14 -0700

    Add: combination-target-sum - submission-12

---
+++ Data Structures & Algorithms/combination-target-sum/submission-12.py
@@ -0,0 +1,24 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        res = []
+        nums.sort()
+
+        def dfs(start, path, remaining):
+            # return all possible combination = target when start from nums[start], and historical path as path (not including current number)
+            if remaining == 0:
+                res.append(path.copy())
+                return
+            
+            for i in range(start, len(nums)):
+                # choose
+                # pruning:
+                    if remaining - nums[i] < 0:
+                        break
+                    path.append(nums[i])
+                    # explore
+                    dfs(i, path, remaining - nums[i])
+                    #undo
+                    path.pop()
+
+        dfs(0, [], target)
+        return res
