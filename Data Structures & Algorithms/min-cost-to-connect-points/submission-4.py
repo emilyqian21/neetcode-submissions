ec43ec0150ad60b627b81e4f71eb57ece98a7bd7
emilyqian21 <47677144+emilyqian21@users.noreply.github.com>
2026-09-03 22:21:17 -0700

    Add: min-cost-to-connect-points - submission-4

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-4.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+    # prim's algo
+    # 每次都计算和每个点之间的距离，然后找最近的点
+    #Prim + heap version
+
+    # Time:  O(n² log n) # 一共有n^2 edge, 每次heap操作是logn
+    # Space: O(n²) 
+
+        total = 0
+        heap = [(0,0)] # distance from current point to point i, point i
+        visit = set()
+
+        while heap:
+            cur_dist,cur_point = heapq.heappop(heap)
+            if cur_point in visit:
+                continue
+            total += cur_dist
+            visit.add(cur_point)
+
+            # if len(visit) == len(points):
+            #     return total
+
+            # explore neighbors for next point move
+            for i in range(len(points)): #当前新加入的 node 可以连接所有其他点。
+                if i in visit:
+                    continue
+                dist = abs(points[i][0] - points[cur_point][0]) + abs( points[i][1] - points[cur_point][1])
+                heapq.heappush(heap, (dist, i))
+        return total
