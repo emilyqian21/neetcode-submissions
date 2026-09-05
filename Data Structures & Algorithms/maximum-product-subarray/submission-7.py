c95e093e42c4428cc46272886c61c8a1b399e0e7
emilyqian21 <47677144+emilyqian21@users.noreply.github.com>
2026-09-04 20:28:08 -0700

    Add: maximum-product-subarray - submission-7

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-7.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        # kadane's algo
+        cur_max = nums[0]
+        cur_min = nums[0]
+        res_max = nums[0]
+
+        for i in range(1,len(nums)):
+            temp_max = cur_max
+            temp_min = cur_min
+ 
+            cur_max = max(temp_max * nums[i], temp_min * nums[i], nums[i])
+            cur_min = min(temp_max * nums[i], temp_min * nums[i], nums[i])
+
+            res_max = max(cur_max, cur_min, res_max)
+         
+        return res_max
