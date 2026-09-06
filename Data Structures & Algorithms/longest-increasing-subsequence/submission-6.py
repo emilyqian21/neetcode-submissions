66effaaae76166c8de51f428767caa5da9ea00d2
emilyqian21 <47677144+emilyqian21@users.noreply.github.com>
2026-09-05 21:38:00 -0700

    Add: longest-increasing-subsequence - submission-6

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-6.py
@@ -0,0 +1,17 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        # bottom up dp (otpimal solution is binary search + greedy)
+        # time: O(n^2)
+        # space: O(n)
+        
+        # 定义： dp[i] = 以 nums[i] 结尾的最长严格递增子序列长度
+        # 核心公式： if nums[j] < nums[i]: dp[i] = max(dp[i], dp[j] + 1) 如果这个数比之前的数大，那可以接在之前的数上面形成新的LIS
+
+        n = len(nums)
+        dp = [1] * n # 必须以 nums[i] 结尾的最长 increasing subsequence 长度，包含nums[i]，所以初始值都是 1。最短自己就是一个subsequence
+        #dp[0] = 1
+        for i in range(1,n):
+            for j in range(i): # 在 i 之前的每一个数字
+                if nums[i] > nums[j]: # 如果 nums[i] 大于 之前的数字nums[j]
+                    dp[i] = max(dp[i], dp[j] + 1) # we don't want to reduce dp[i],so need to use max. 
+        return max(dp) # 不是dp[-1] 因为 LIS 不一定以最后一个 number 结尾。
