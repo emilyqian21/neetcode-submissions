869f6cdd2855b94a39a3bef7a337ec0f1a975bb4
emilyqian21 <47677144+emilyqian21@users.noreply.github.com>
2026-09-07 11:06:04 -0700

    Add: insert-new-interval - submission-7

---
+++ Data Structures & Algorithms/insert-new-interval/submission-7.py
@@ -0,0 +1,21 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        # 和new interval 比，在new interval 之前，中间，之后
+
+        res = []
+
+        i = 0 
+        while i < len(intervals) and intervals[i][1] < newInterval[0]: # no overlap
+                res.append(intervals[i])
+                i += 1
+
+        while i < len(intervals) and intervals[i][0] <= newInterval[1]:
+                newInterval = [min(intervals[i][0], newInterval[0]), max(intervals[i][1], newInterval[1])]
+                i += 1
+        res.append(newInterval)
+
+        while i < len(intervals):
+                res.append(intervals[i])
+                i += 1
+
+        return res                
